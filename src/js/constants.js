9d9bafd07dc4cfdb30613ede5a9e45867ff8fed6
cartory <xXcartoryXx@gmail.com>
2021-07-17 21:12:34 -0400

    npm enCoderr

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,14 +1,5 @@
+import { encode, decode } from 'encoderr'
 import { TranspositionCipher } from './transCol'
-
-import {
-    caesarEncode, vigenereEncode, zigzagEncode,
-    monoEncode, seriesEncode, columnEncode, rowEncode
-} from './encoding'
-
-import {
-    caesarDecode, vigenereDecode, zigzagDecode,
-    monoDecode, seriesDecode, columnDecode, rowDecode
-} from './decoding'
 
 const vlada = new TranspositionCipher()
 
@@ -16,39 +7,39 @@ export const encoding = {
     caesar: ([alpha, text, moves]) => {
         moves = Number.parseInt(moves)
         if (!moves || moves < 1) return 'Number Required'
-        return caesarEncode(alpha, text, moves)
+        return encode.caesarEncode(alpha, text, moves)
     },
 
     vigenere: ([alpha, text, key]) => {
         if (key.length < 1) return 'Alpha String Required'
-        return vigenereEncode(alpha, text, key)
+        return encode.vigenereEncode(alpha, text, key)
     },
 
     zigzag: ([_, text, cols]) => {
         cols = Number.parseInt(cols)
         if (!cols || cols < 1) return 'Number Required'
-        return zigzagEncode(text, cols)
+        return encode.zigzagEncode(text, cols)
     },
 
     mono: ([alpha, text, alphaCrypt]) => {
         if (alpha.length !== alphaCrypt.length) return "Alpha(s) don't Match"
-        return monoEncode(alpha, text.toLowerCase(), alphaCrypt)
+        return encode.monoEncode(alpha, text.toLowerCase(), alphaCrypt)
     },
 
     series: ([_, text]) => {
-        let { arrayIndex, code } = seriesEncode(text)
+        let { arrayIndex, code } = encode.seriesEncode(text)
         sessionStorage.setItem('series', JSON.stringify(arrayIndex))
         return code
     },
 
     col: ([_, text, key]) => {
         if (key.length < 1) return 'Alpha String Required'
-        return columnEncode(text, key)
+        return encode.columnEncode(text, key)
     },
 
     row: ([_, text, key]) => {
         if (key.length < 1) return 'Alpha String Required'
-        return rowEncode(text.toLowerCase(), key)
+        return encode.rowEncode(text.toLowerCase(), key)
     },
 
     colNum: ([_, text, key]) => {
@@ -62,34 +53,34 @@ export const decoding = {
     caesar: ([alpha, text, moves]) => {
         moves = Number.parseInt(moves)
         if (!moves && moves !== 0) return 'Number Required'
-        return caesarDecode(alpha, text, moves)
+        return decode.caesarDecode(alpha, text, moves)
     },
     vigenere: ([alpha, text, key]) => {
         if (key.length < 1) return 'Alpha String Required'
-        return vigenereDecode(alpha, text, key)
+        return decode.vigenereDecode(alpha, text, key)
     },
     zigzag: ([_, text, cols]) => {
         cols = Number.parseInt(cols)
         if (!cols || cols < 1) return 'Number Required'
-        return zigzagDecode(text, cols)
+        return decode.zigzagDecode(text, cols)
     },
     mono: ([alpha, text, alphaCrypt = alpha2]) => {
         if (alpha.length !== alphaCrypt.length) return "Alpha(s) don't Match"
-        return monoDecode(alpha, text.toLowerCase(), alphaCrypt)
+        return decode.monoDecode(alpha, text.toLowerCase(), alphaCrypt)
     },
     series: ([_, text]) => {
         let arrayIndex = sessionStorage.getItem('series')
         if (!arrayIndex) return 'Needing encode First For Matching'
-        return seriesDecode(text, JSON.parse(arrayIndex))
+        return decode.seriesDecode(text, JSON.parse(arrayIndex))
     },
     col: ([alpha, text, key]) => {
         if (key.length < 1 || alpha.length < 1) return 'Alpha String Required'
-        return columnDecode(text, key)
+        return decode.columnDecode(text, key)
     },
 
     row: ([alpha, text, key]) => {
         if (key.length < 1 || alpha.length < 1) return 'Alpha String Required'
-        return rowDecode(text.toLowerCase(), key)
+        return decode.rowDecode(text.toLowerCase(), key)
     },
 
     colNum: ([_, text, key]) => {
